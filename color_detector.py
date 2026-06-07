9fe19d52cac8f32b8dddebd46e92085193bb7cc0
JunaydEco-coder <mohammedjunaydali@gmail.com>
2026-06-07 17:09:57 +0530

    Fix boundary hue values being misclassified as unknown

--- color_detector.py
+++ color_detector.py
@@ -1,19 +1,16 @@
-
 def get_color(h, s, v):
     if v < 50:
         return "black"
     if s < 50 and v > 200:
         return "white"
-
     if h < 10 or h > 160:
         return "red"
-    if 10 < h < 25:
+    if 10 <= h < 25:
         return "orange"
-    if 25 < h < 35:
+    if 25 <= h < 35:
         return "yellow"
-    if 35 < h < 85:
+    if 35 <= h < 85:
         return "green"
-    if 85 < h < 130:
+    if 85 <= h < 130:
         return "blue"
-
     return "unknown"
